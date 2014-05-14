20c561f912cc40767198dfa9b545d04d5370d96a
Allen90 <90allen90@gmail.com>
2014-05-14 14:00:11 +0200

    Primo Commit

---
+++ src/Test/TestClass.java
@@ -0,0 +1,15 @@
+package Test;
+
+import server.logica.slotmachine.Slot;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		Slot s = new Slot();
+		
+		System.out.println(s.getCombinazione());
+
+	}
+
+}
---
+++ src/server/logica/slotmachine/Slot.java
@@ -0,0 +1,22 @@
+package server.logica.slotmachine;
+
+import java.util.Random;
+
+public class Slot {
+
+	private Combinazione c = null;
+
+	
+	public Slot(){
+		c = new Combinazione();
+		
+	}
+	
+	public String getCombinazione(){
+		c.ricalcola();
+		Random r = new Random();
+		
+		return c.getValori();		
+	}
+	
+}
