b423ab06bbbf06dccf3407b9e18d962e65660af3
Fritz90 <xxfritz.90xx@gmail.com>
2014-05-27 14:11:11 +0200

    server start e socket login

--- src/codePartite/LobbyRubaMazzo.java
+++ src/codePartite/LobbyRubaMazzo.java
@@ -1,5 +1,32 @@
 package codePartite;
+import java.util.ArrayList;
 
+import model.Client;
+import model.Utente;
 public class LobbyRubaMazzo {
-
+	private ArrayList<Client> client;
+	private static LobbyRubaMazzo l;
+	
+	private LobbyRubaMazzo(){
+		client = new ArrayList<Client>();
+	}
+	
+	public static LobbyRubaMazzo getInstance(){
+		if(l == null)
+			l = new LobbyRubaMazzo();
+		return l;
+	}
+	
+	public ArrayList<Client> getLobbyRubaMazzo(){
+		return client;
+	}
+	
+	public void svuotaLobbyRubaMazzo(){
+		for(int i = 0;i< client.size();i++)
+			client.remove(i);
+	}
+	
+	public void addUserLobbyRubaMazzo(Client c){
+		client.add(c);
+	}
 }
--- src/codePartite/LobbyTombola.java
+++ src/codePartite/LobbyTombola.java
@@ -1,5 +1,35 @@
 package codePartite;
 
-public class LobbyTombola {
+import java.util.ArrayList;
 
+import model.Client;
+import model.Utente;
+
+public class LobbyTombola {
+	private static LobbyTombola l;
+	private ArrayList<Client> client;
+	private LobbyTombola(){
+		client = new ArrayList<Client>();
+	}
+	
+	public LobbyTombola getIstance(){
+		if(l == null)
+			l = new LobbyTombola();
+		return l;
+	}
+	
+	public int numUtentiTombola(){
+		return client.size();
+	}
+	
+	public void svuotaLobbyTombola(){
+		for(int i = 0;i < client.size();i++)
+			client.remove(i);
+	}
+	
+	public void addUserLobbyTomb(Client c,int i){
+		GiocatoreTombola g = new GiocatoreTombola(c,i);
+		lobbyTombola.add(g);
+	}
+	
 }
