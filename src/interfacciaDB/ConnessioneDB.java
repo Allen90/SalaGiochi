a5b6fc51969316a863205485621ccb39a2a17a5d
Allen90 <90allen90@gmail.com>
2014-05-27 16:25:57 +0200

    Implementazione metodi Interfaccia DB

--- src/interfacciaDB/ConnessioneDB.java
+++ src/interfacciaDB/ConnessioneDB.java
@@ -1,6 +1,12 @@
 package interfacciaDB;
 
 import java.sql.*;
+import java.util.ArrayList;
+
+import eccezioni.EccezioneClassificaVuota;
+import eccezioni.EccezioneUtente;
+import eccezioni.EccezioneUtenteNonTrovato;
+import model.Utente;
 
 public class ConnessioneDB {
 
@@ -10,6 +16,8 @@ public class ConnessioneDB {
 	private static final String PATH="jdbc:derby:DBSalagiochi";
 	private static final String USER="user";
 	private static final String PWD="pass";
+	private static String DBErr_1 = "Errore nel caricamento del Database!";
+	private static String DBErr_2 = "Errore nella modifica del Database!";
 	
 	private ConnessioneDB() {  
           
@@ -45,9 +53,7 @@ public class ConnessioneDB {
     	PreparedStatement ps = null;
     	try{    		
     		ps = con.prepareStatement(s);
-    		System.out.println("statement creato con la connessione");
     	}catch(SQLException e){
-    		System.out.println("errore\n");
     		e.printStackTrace();
     	}
     	return ps;
@@ -60,4 +66,194 @@ public class ConnessioneDB {
     		e.printStackTrace();
     	}
     }
+    
+    public boolean addUtente(Utente utente){
+    	ConnessioneDB cdb = null;
+    	PreparedStatement ps = null;
+    	boolean ok = true;
+    	try{
+    		cdb = ConnessioneDB.getInstance();
+    		ps = cdb.getPStatement(StatementsDB.aggiungiUtente);
+    		ps.setString(1, utente.getUsername());
+    		ps.setString(2, utente.getPassword());
+    		ps.setString(3, utente.getNome());
+    		ps.setString(4, utente.getCognome());
+    		ps.execute();
+    	}catch(SQLException e){
+    		e.printStackTrace();
+    		ok = false;
+    	}
+    	try {
+			ps.close();
+			closeDB();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			ok = false;
+		}
+    	return ok;
+    }
+    
+    private Utente getUtente(String username) throws EccezioneUtenteNonTrovato{
+    	ConnessioneDB cdb = null;
+    	PreparedStatement ps = null;
+    	ResultSet rs = null;
+    	Utente utente = null;
+    
+    	try {
+        	cdb = getInstance();
+        	ps = cdb.getPStatement(StatementsDB.getUtente);
+			ps.setString(1, username);
+			rs = ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+
+    	try {
+			while(rs.next()){
+				utente = new Utente(rs.getString("nome"), rs.getString("cognome"),
+						rs.getString("userid"), rs.getString("password"), rs.getInt("crediti"));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (EccezioneUtente e) {
+			e.printStackTrace();
+		}
+
+    	try {
+    		rs.close();
+			ps.close();
+			closeDB();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+    	if(utente == null)
+    		throw new EccezioneUtenteNonTrovato("Utente non trovato : " + username);
+    	else return utente;
+    }
+    
+    public boolean controlloUtente(String username, String password){
+    	Utente utente = null;
+    	
+    	try{
+    		utente = getUtente(username);
+    	}catch(EccezioneUtenteNonTrovato e){
+    		e.printStackTrace();
+    		return false;
+    	}
+    	
+    	if(utente.getPassword().equals(password))
+    		return true;
+    	else return false;
+    }
+    
+    public boolean aggiornaCrediti(int premio, String username){
+    	ConnessioneDB cdb = null;
+    	PreparedStatement ps = null;
+    	boolean ok = true;
+    	
+    	try{
+    		cdb = ConnessioneDB.getInstance();
+    		ps = cdb.getPStatement(StatementsDB.aggiornaCrediti);
+    		ps.setInt(1, premio);
+    		ps.setString(2, username);
+    		ps.execute();
+    	}catch(SQLException e){
+    		e.printStackTrace();
+    		ok = false;
+    	}
+    	
+    	try {
+			ps.close();
+			closeDB();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			ok = false;
+		}
+    	return ok;
+    }
+    
+    public boolean resetCreditiG(String username){
+    	ConnessioneDB cdb = null;
+    	PreparedStatement ps = null;
+    	boolean ok = true;
+    	
+    	try{
+    		cdb = ConnessioneDB.getInstance();
+    		ps = cdb.getPStatement(StatementsDB.resetCreditiGiornalieri);
+    		ps.setString(1, username);
+    		ps.execute();
+    	}catch(SQLException e){
+    		e.printStackTrace();
+    		ok = false;
+    	}
+    	
+    	try {
+			ps.close();
+			closeDB();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			ok = false;
+		}
+    	return ok;
+    }
+    
+    public ArrayList<Utente> getClassifica(boolean isGiornaliera) throws EccezioneClassificaVuota{
+    	ArrayList<Utente> classifica = new ArrayList<Utente>();
+    	ConnessioneDB cdb = null;
+    	Statement st = null;
+    	ResultSet rs = null;
+    	
+    	try{
+    		cdb = ConnessioneDB.getInstance();
+    		st = cdb.getStatement();
+    		if(isGiornaliera)
+    			rs = st.executeQuery(StatementsDB.getClassificaGiornaliera);
+    		else rs = st.executeQuery(StatementsDB.getClassifica);
+    	}catch(SQLException e){
+    		e.printStackTrace();
+    	}
+    	
+    	try{
+			while(rs.next()){
+				classifica.add(new Utente(rs.getString("nome"), rs.getString("cognome"),
+						rs.getString("userid"), rs.getString("password"), rs.getInt("crediti")));
+			}
+    	}catch (SQLException e) {
+			e.printStackTrace();
+		}catch (EccezioneUtente e) {
+			e.printStackTrace();
+		}
+
+    	try {
+    		rs.close();
+			st.close();
+			closeDB();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+    	
+    	if(classifica == null)
+    		throw new EccezioneClassificaVuota("Classifica vuota! Nessun utente registrato!");
+    	else return classifica;
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
