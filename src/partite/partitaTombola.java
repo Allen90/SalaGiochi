717f376ec42406475bccb08c4d79cedd0d15aca2
Allen90 <90allen90@gmail.com>
2014-05-31 15:40:07 +0200

    aggiunta PartitaTombola

--- src/partite/partitaTombola.java
+++ src/partite/partitaTombola.java
@@ -1,5 +1,26 @@
 package partite;
 
-public class partitaTombola {
+import java.util.ArrayList;
+
+import Tombola.GiocatoreTombola;
+import Tombola.Tabellone;
+
+public class partitaTombola implements Runnable{
+
+	ArrayList<GiocatoreTombola> giocatori = null;
+	Tabellone tabellone = null;
+	
+	public partitaTombola(ArrayList<GiocatoreTombola> giocatori){
+		tabellone = new Tabellone();
+		this.giocatori = giocatori;
+		for(GiocatoreTombola giocatore: giocatori)
+			giocatore.aggiornaTabellone(tabellone);
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
