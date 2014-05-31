0fa5adb8bc492ba7c7a7aa5900c878cdebd68e9b
Allen90 <90allen90@gmail.com>
2014-05-31 15:35:43 +0200

    Aggiornamento modello Tombola

--- src/Tombola/GiocatoreTombola.java
+++ src/Tombola/GiocatoreTombola.java
@@ -2,14 +2,26 @@ package Tombola;
 
 import java.util.ArrayList;
 
+import model.Utente;
+
 public class GiocatoreTombola {
 	
 	private ArrayList<Tabella> cartelle = null;
-	private Client client = null;
+	private Utente utente = null;
+	private Tabellone tabellone = null;
 	
-	public GiocatoreTombola(ArrayList<Tabella> cartelle, Client client){
-		this.client = client;
+	
+	public GiocatoreTombola(ArrayList<Tabella> cartelle, Utente giocatore ){
+		this.utente = giocatore;
 		this.cartelle = new ArrayList<Tabella>(cartelle);
+	}
+	
+	public void aggiornaTabellone(Tabellone t){
+		tabellone = t;
+	}
+	
+	public Utente getUtente(){
+		return utente;
 	}
 	
 	
