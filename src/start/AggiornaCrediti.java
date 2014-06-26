d4c43fbfc117d6d1568ffba9e26460af90b0f55b
Fritz90 <xxfritz.90xx@gmail.com>
2014-06-26 09:36:28 +0200

    ultimo push

--- src/start/AggiornaCrediti.java
+++ src/start/AggiornaCrediti.java
@@ -1,8 +1,10 @@
 package start;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import userModel.Utente;
 import eccezioni.EccezioneClassificaVuota;
 import interfacciaDB.ConnessioneDB;
 
@@ -26,7 +28,10 @@ public class AggiornaCrediti implements Runnable{
 	}
 	
 	public void run() {
-//		Date d = new Date();
+		Date d = new Date();
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		int day = cal.get(Calendar.DAY_OF_WEEK_IN_MONTH);
 		while(continua){
 		try {
 			Thread.sleep(3600000);
@@ -34,8 +39,22 @@ public class AggiornaCrediti implements Runnable{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-//		Date d2 = new Date();
-//		Calendar c = Calendar.
+		Date d2 = new Date();
+		Calendar cal2 = Calendar.getInstance();
+		cal2.setTime(d2);
+		int day2 = cal2.get(Calendar.DAY_OF_MONTH);
+		if(day != day2 ){
+			ArrayList<Utente> utenti;
+			try {
+				utenti = db.getClassifica(true);
+				for(int i = 0;i< utenti.size();i++)
+					db.resetCreditiG(utenti.get(i).getUsername());
+			} catch (EccezioneClassificaVuota e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 		try {
 			db.aggiornaPeriodico(1);
 		} catch (EccezioneClassificaVuota e) {
